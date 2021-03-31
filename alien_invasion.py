db8af6cae979c567a784d3fea9c91b5cf0cbd69e
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-03-30 21:37:31 -0700

    Piloting the ship by responding to a right arrow key

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,15 +29,15 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1 
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
