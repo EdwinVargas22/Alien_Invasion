fe08695f332f45301ebcf93e82bc1458e1b1f256
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-04-03 14:11:19 -0700

    moving the aliens to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,6 +38,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -51,6 +52,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
