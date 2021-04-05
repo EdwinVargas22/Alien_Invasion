b7efc6250597b65bc04a5a939f2f8fab413dc6c6
EdwinVargas22 <edwin.vargas9662@gmail.com>
2021-04-04 18:12:18 -0700

    displaying the level

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,6 +87,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
+
     def _update_aliens(self): 
         """
         check if the fleet is at an edge,
@@ -149,6 +153,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
